34a06dbd4f0c695c1da1db20ecebb14902506e4e
Gelasio Ebel Junior <gelasio.e@aluno.ifsc.edu.br>
2024-10-24 05:46:11 -0300

    Merge remote-tracking branch 'origin/master'

--- app/src/test/java/br/ifsc/edu/br/gelasio/ddm/ExampleUnitTest.java
+++ app/src/test/java/br/ifsc/edu/br/gelasio/ddm/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package br.ifsc.edu.br.gelasio.ddm;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
